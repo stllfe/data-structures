19da3e136fef17187b738fe7103106b1e8f0962e
stllfe <pavlovi4.o@gmail.com>
2020-10-10 12:36:32 +0700

    Added Circular Queue class. Changed the binary_tree.py a little. Mainly refactoring, nothing special.

--- src/trees/binary_tree.py
+++ src/trees/binary_tree.py
@@ -1,40 +1,65 @@
-from source.queue import Queue
+from collections import deque
 
 
 class Node:
 
     def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+        self._value = value
+        self._left = left
+        self._right = right
+
+    def __str__(self):
+        return f"<Node ('{type(self._value).__name__}'): {self._value}>"
+
+    def __bool__(self):
+        return True
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self._right
 
     @property
     def is_leaf(self):
-        return not self.left and not self.right
+        return not self._left and not self._right
 
     @property
     def children(self):
-        children = list()
-        for child in (self.left, self.right):
-            if child:
-                children.append(child)
-        return children
+        return [child for child in (self._left, self._right) if child]
 
 
 class BinarySearchTree:
 
     def __init__(self):
-        self.root = None
+        self._root = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def __bool__(self):
+        return self._root is not None
+
+    @property
+    def root(self) -> Node:
+        return self._root
 
     def _insert_at(self, node, value):
         if not node:
             return Node(value)
 
         if value < node.value:
-            node.left = self._insert_at(node.left, value)
+            node._left = self._insert_at(node._left, value)
 
         if value > node.value:
-            node.right = self._insert_at(node.right, value)
+            node._right = self._insert_at(node._right, value)
 
         return node
 
@@ -51,7 +76,7 @@ class BinarySearchTree:
         return node
 
     def _remove_at(self, node, value):
-        # case 1: if it contains value and at the same time has no children, we just replace it with None
+        # Case 1: If it contains value and at the same time has no children, we just replace it with `None`.
         if node.is_leaf:
             return None
 
@@ -63,21 +88,21 @@ class BinarySearchTree:
             node.right = self._remove_at(node.right, value)
             return node
 
-        # value == node.value, then we can start trimming
-        # case 2, 3: only one subtree, then just replace with its child
+        # value == node.value, then we can start trimming.
+        # Case 2, 3: Only one subtree, then just replace with its child.
         if node.left and not node.right:
             return node.left
 
         if node.right and not node.left:
             return node.right
 
-        # case 4: justify which child to replace with
-        # actually it can be either the largest from the left subtree or the smallest from the right one
-        # we don't really care, so let's do the latter
+        # Case 4: Justify which child to replace with.
+        # Actually it can be either the largest from the left subtree or the smallest from the right one.
+        # We don't really care, so let's do the latter.
         replacement = self._find_min(node.right)
-        # that's cool, but now we need to delete this `replacement` node from the tree to avoid duplicate values
-        # fortunately it will always be case 2, 3 node, since we went all the way down to the left to get it
-        # hence, we won't get infinite recursion, so it's safe
+        # That's cool, but now we need to delete this `replacement` node from the tree to avoid duplicate values.
+        # Fortunately it will always be case 2, 3 node, since we went all the way down to the left to get it.
+        # Hence, we won't get infinite recursion, so it's safe.
         node = self._remove_at(node, replacement.value)
         node.value = replacement.value
         return node
@@ -122,60 +147,57 @@ class BinarySearchTree:
         if self.contains(value):
             return False
 
-        self.root = self._insert_at(self.root, value)
+        self._root = self._insert_at(self._root, value)
+        self._size += 1
         return True
 
     def remove(self, value):
         if not self.contains(value):
             return False
 
-        self.root = self._remove_at(self.root, value)
+        self._root = self._remove_at(self._root, value)
         return True
 
     def find(self, value):
-        return self._find_at(self.root, value)
+        return self._find_at(self._root, value)
 
-    def contains(self, value):
+    def contains(self, value) -> bool:
         return self.find(value) is not None
 
-    def preorder(self):
-        return self._preorder_from(self.root)
+    def preorder(self) -> list:
+        return self._preorder_from(self._root)
 
-    def inorder(self):
-        return self._inorder_from(self.root)
+    def inorder(self) -> list:
+        return self._inorder_from(self._root)
 
-    def postorder(self):
-        return self._postorder_from(self.root)
+    def postorder(self) -> list:
+        return self._postorder_from(self._root)
 
-    def level_order(self):
+    def levelorder(self) -> list:
         if not self.root:
-            return
+            return []
 
         levels = list()
-        q = Queue(first=[self.root])
+        queue = deque()
+        queue.append([self.root])
+
         levels.append([self.root.value])
 
-        while q.is_not_empty:
-            nodes = q.dequeue()
+        while queue:
+            nodes = queue.popleft()
             level = list()
             children = list()
             for node in nodes:
                 children.extend(node.children)
                 for child in node.children:
                     level.append(child.value)
-
             if not children:
                 break
-
-            q.enqueue(children)
+            queue.append(children)
             levels.append(level)
 
         return levels
 
-    def print(self):
-        # todo: implement me
-        levels = self.level_order()
-        for level in levels:
-            pass
-
-
+    def pretty(self) -> str:
+        # Todo: implement me!
+        pass
