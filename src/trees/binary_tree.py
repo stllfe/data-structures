27884a0ffc379d71aef2661d6b7552e59140bf5b
stllfe <pavlovi4.o@gmail.com>
2020-11-01 14:16:07 +0700

    Simplified Node class structure. Added root setter for BinaryTree class. Solved max_depth problem with two approaches. Testd on LeetCode

--- src/trees/binary_tree.py
+++ src/trees/binary_tree.py
@@ -4,45 +4,29 @@ from collections import deque
 class Node:
 
     def __init__(self, value, left=None, right=None):
-        self._value = value
-        self._left = left
-        self._right = right
+        self.value = value
+        self.left = left
+        self.right = right
 
     def __str__(self):
-        return f"<Node ('{type(self._value).__name__}'): {self._value}>"
+        return f"<Node ('{type(self.value).__name__}'): {self.value}>"
 
     def __bool__(self):
         return True
 
     @property
-    def value(self):
-        return self._value
-
-    @property
-    def left(self):
-        return self._left
-
-    @property
-    def right(self):
-        return self._right
-
-    @property
     def is_leaf(self):
-        return not self._left and not self._right
+        return not self.left and not self.right
 
     @property
     def children(self):
-        return [child for child in (self._left, self._right) if child]
+        return [child for child in (self.left, self.right) if child]
 
 
-class BinarySearchTree:
+class BinaryTree:
 
     def __init__(self):
         self._root = None
-        self._size = 0
-
-    def __len__(self):
-        return self._size
 
     def __bool__(self):
         return self._root is not None
@@ -51,153 +35,169 @@ class BinarySearchTree:
     def root(self) -> Node:
         return self._root
 
-    def _insert_at(self, node, value):
-        if not node:
-            return Node(value)
-
-        if value < node.value:
-            node._left = self._insert_at(node._left, value)
-
-        if value > node.value:
-            node._right = self._insert_at(node._right, value)
-
-        return node
-
-    def _find_at(self, node, value):
-        if not node:
-            return None
-
-        if value < node.value:
-            return self._find_at(node.left, value)
-
-        if value > node.value:
-            return self._find_at(node.right, value)
-
-        return node
-
-    def _remove_at(self, node, value):
-        # Case 1: If it contains value and at the same time has no children, we just replace it with `None`.
-        if node.is_leaf:
-            return None
-
-        if value < node.value:
-            node.left = self._remove_at(node.left, value)
-            return node
-
-        if value > node.value:
-            node.right = self._remove_at(node.right, value)
-            return node
-
-        # value == node.value, then we can start trimming.
-        # Case 2, 3: Only one subtree, then just replace with its child.
-        if node.left and not node.right:
-            return node.left
-
-        if node.right and not node.left:
-            return node.right
-
-        # Case 4: Justify which child to replace with.
-        # Actually it can be either the largest from the left subtree or the smallest from the right one.
-        # We don't really care, so let's do the latter.
-        replacement = self._find_min(node.right)
-        # That's cool, but now we need to delete this `replacement` node from the tree to avoid duplicate values.
-        # Fortunately it will always be case 2, 3 node, since we went all the way down to the left to get it.
-        # Hence, we won't get infinite recursion, so it's safe.
-        node = self._remove_at(node, replacement.value)
-        node.value = replacement.value
-        return node
-
-    def _find_min(self, node):
-        if not node.left:
-            return node
-
-        return self._find_min(node.left)
-
-    def _find_max(self, node):
-        if not node.right:
-            return node
-
-        return self._find_max(node.right)
-
-    def _preorder_from(self, node):
-        result = []
-        if node:
-            result.append(node.value)
-            result += self._preorder_from(node.left)
-            result += self._preorder_from(node.right)
-        return result
-
-    def _inorder_from(self, node):
-        result = []
-        if node:
-            result += self._inorder_from(node.left)
-            result.append(node.value)
-            result += self._inorder_from(node.right)
-        return result
-
-    def _postorder_from(self, node):
-        result = []
-        if node:
-            result += self._postorder_from(node.left)
-            result += self._postorder_from(node.right)
-            result.append(node.value)
-        return result
-
-    def insert(self, value):
-        if self.contains(value):
-            return False
-
-        self._root = self._insert_at(self._root, value)
-        self._size += 1
-        return True
-
-    def remove(self, value):
-        if not self.contains(value):
-            return False
-
-        self._root = self._remove_at(self._root, value)
-        return True
-
-    def find(self, value):
-        return self._find_at(self._root, value)
-
-    def contains(self, value) -> bool:
-        return self.find(value) is not None
+    @root.setter
+    def root(self, value):
+        assert isinstance(value, Node)
+        if self._root:
+            raise AttributeError("Root value is already set.")
+        self._root = value
 
     def preorder(self) -> list:
-        return self._preorder_from(self._root)
+
+        def preorder_from(node):
+            result = []
+            if node:
+                result.append(node.value)
+                result += preorder_from(node.left)
+                result += preorder_from(node.right)
+            return result
+
+        return preorder_from(self.root)
 
     def inorder(self) -> list:
-        return self._inorder_from(self._root)
+
+        def inorder_from(node):
+            result = []
+            if node:
+                result += inorder_from(node.left)
+                result.append(node.value)
+                result += inorder_from(node.right)
+            return result
+
+        return inorder_from(self.root)
 
     def postorder(self) -> list:
-        return self._postorder_from(self._root)
+
+        def postorder_from(node):
+            result = []
+            if node:
+                result += postorder_from(node.left)
+                result += postorder_from(node.right)
+                result.append(node.value)
+            return result
+
+        return postorder_from(self.root)
 
     def levelorder(self) -> list:
         if not self.root:
             return []
 
-        levels = list()
+        # Queue for storing nodes to travers.
         queue = deque()
         queue.append([self.root])
 
-        levels.append([self.root.value])
+        # List to store the traversed nodes' values.
+        values = list()
+        values.append([self.root.value])
 
         while queue:
             nodes = queue.popleft()
-            level = list()
-            children = list()
-            for node in nodes:
-                children.extend(node.children)
-                for child in node.children:
-                    level.append(child.value)
-            if not children:
-                break
-            queue.append(children)
-            levels.append(level)
+            subtrees = list()
 
-        return levels
+            for node in nodes:
+                subtrees.extend(node.children)
+
+            if subtrees:
+                queue.append(subtrees)
+                values.append([node.val for node in subtrees])
+
+        return values
 
     def pretty(self) -> str:
         # Todo: implement me!
         pass
+
+
+class BinarySearchTree(BinaryTree):
+
+    def __init__(self):
+        super().__init__()
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def insert(self, value):
+        if self.contains(value):
+            return False
+
+        def insert_at(node, value):
+            if not node:
+                return Node(value)
+
+            if value < node.value:
+                node.left = insert_at(node.left, value)
+
+            if value > node.value:
+                node.right = insert_at(node.right, value)
+
+            return node
+
+        self._root = insert_at(self.root, value)
+        self._size += 1
+        return True
+
+    def remove(self, value, swap_rightmost=True):
+        if not self.contains(value):
+            return False
+
+        def find_min(node):
+            if not node.left:
+                return node
+            return find_min(node.left)
+
+        def find_max(node):
+            if not node.right:
+                return node
+            return find_max(node.right)
+
+        def remove_at(node, value):
+            # Case 1: If it contains value and at the same time has no children, we just replace it with `None`.
+            if node.is_leaf:
+                return None
+
+            if value < node.value:
+                node.left = remove_at(node.left, value)
+                return node
+
+            if value > node.value:
+                node.right = remove_at(node.right, value)
+                return node
+
+            # value == node.value, then we can start trimming.
+            # Case 2, 3: Only one subtree, then just replace with its child.
+            if node.left and not node.right:
+                return node.left
+
+            if node.right and not node.left:
+                return node.right
+
+            # Case 4: Justify which child to replace with.
+            # Actually it can be either the largest from the left subtree or the smallest from the right one.
+            # We don't really care.
+            replacement = find_min(node.right) if swap_rightmost else find_max(node.left)
+            # That's cool, but now we need to delete this `replacement` node from the tree to avoid duplicate values.
+            # Fortunately it will always be case 2, 3 node, since we went all the way down to the left to get it.
+            # Hence, we won't get infinite recursion, so it's safe.
+            node = remove_at(node, replacement.value)
+            node.value = replacement.value
+            return node
+
+        self._root = remove_at(self._root, value)
+        return True
+
+    def find(self, value):
+
+        def find_at(node, value):
+            if not node:
+                return None
+            if value < node.value:
+                return find_at(node.left, value)
+            if value > node.value:
+                return find_at(node.right, value)
+            return node
+
+        return find_at(self._root, value)
+
+    def contains(self, value) -> bool:
+        return self.find(value) is not None
---
+++ src/trees/problems/max_depth.py
@@ -0,0 +1,47 @@
+from src.trees import BinaryTree
+from src.trees.binary_tree import Node
+
+
+def max_depth(tree: BinaryTree, top_down=True) -> int:
+    """Given a binary tree, finds its maximum depth.
+
+    The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+    Note:
+        A leaf is a node with no children.
+
+    Example:
+        Given binary tree [3,9,20,null,null,15,7]
+            3
+           / \
+          9  20
+            /  \
+           15   7
+        Returns its depth = 3
+
+    Args:
+        tree (BinaryTree): The tree to find max depth of.
+        top_down (bool): Whether to solve using top-down approach (preorder) or bottom-up instead (postorder).
+    """
+
+    def top_down_solution(node: Node, current_depth: int):
+        if not node:
+            return 0
+        current_depth += 1
+        left_depth = top_down_solution(node.left, current_depth) if node.left else 0
+        right_depth = top_down_solution(node.right, current_depth) if node.right else 0
+        return max(current_depth, left_depth, right_depth)
+
+    def bottom_up_solution(node: Node):
+        if not node:
+            return 0
+        left_depth = bottom_up_solution(node.left)
+        right_depth = bottom_up_solution(node.right)
+        return max(left_depth, right_depth) + 1
+
+    if top_down:
+        return top_down_solution(tree.root, 0)
+
+    return bottom_up_solution(tree.root)
+
+
